5af738954e1f6aa8e8a36fcc7ae758b352e89b19
Brownsley <brownsleyheim@gmail.com>
2026-05-23 18:33:46 +0630

    Fix

--- src/main/java/com/brownsley/my_custom_qr/QrService.java
+++ src/main/java/com/brownsley/my_custom_qr/QrService.java
@@ -1,7 +1,11 @@
 package com.brownsley.my_custom_qr;
 
+import org.springframework.web.client.RestTemplate;
+
 public class QrService {
-    public String generateQr(String data) {
-        return "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + data;
+    public byte[] generateQr(String data) {
+        String url = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + data;
+        RestTemplate template = new RestTemplate();
+        return template.getForObject(url, byte[].class);
     }
 }
