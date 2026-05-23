a9ace6e3fe6d1057fe4665edd4c21d8a2cd2ee1c
Brownsley <brownsleyheim@gmail.com>
2026-05-23 18:49:39 +0630

    Mamory Optomize

--- src/main/java/com/brownsley/my_custom_qr/QrAutoConfiguration.java
+++ src/main/java/com/brownsley/my_custom_qr/QrAutoConfiguration.java
@@ -3,13 +3,18 @@ package com.brownsley.my_custom_qr;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @AutoConfiguration
 public class QrAutoConfiguration {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
     @Bean
     @ConditionalOnMissingBean
-    public QrService qrService() {
-        return new QrService();
+    public QrService qrService(RestTemplate restTemplate) {
+        return new QrService(restTemplate);
     }
 }
--- src/main/java/com/brownsley/my_custom_qr/QrService.java
+++ src/main/java/com/brownsley/my_custom_qr/QrService.java
@@ -3,6 +3,11 @@ package com.brownsley.my_custom_qr;
 import org.springframework.web.client.RestTemplate;
 
 public class QrService {
+    private final RestTemplate template;
+
+    public QrService(RestTemplate template) {
+        this.template = template;
+    }
 
     public byte[] generateQr(String data) {
         return generateQr(data, 150);
@@ -10,7 +15,6 @@ public class QrService {
 
     public byte[] generateQr(String data, int size) {
         String url = "https://api.qrserver.com/v1/create-qr-code/?size=" + size + "x" + size + "&data=" + data;
-        RestTemplate template = new RestTemplate();
         return template.getForObject(url, byte[].class);
     }
 }
